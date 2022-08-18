395be9b46c4d4994d471237a4f95242dda60ada8
salivan78 <s-alivan@yandex.ru>
2022-08-18 21:36:27 +0700

    modify server

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://localhost:5000/'
 //export const API_URL = 'http://192.168.1.66:5000/'
-export const API_URL = 'https://server-test-ep96wzrv6-salivan78.vercel.app'
+export const API_URL = 'https://server-test-zeta.vercel.app'
