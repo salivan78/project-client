7e16a661d69b400b3444c09168be98c802c7b705
salivan78 <s-alivan@yandex.ru>
2022-08-19 19:55:56 +0700

    modify config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://localhost:5000/'
 //export const API_URL = 'http://192.168.1.66:5000/'
-export const API_URL = 'https://server-test-zeta.vercel.app'
+export const API_URL = 'https://server-test-zeta.vercel.app/'
