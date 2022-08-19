4e38c9477e53bccd4368d31f8057b439de2df935
salivan78 <s-alivan@yandex.ru>
2022-08-19 20:33:29 +0700

    update api

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://localhost:5000/'
 //export const API_URL = 'http://192.168.1.66:5000/'
-export const API_URL = 'https://server-test-5iyesex8u-salivan78.vercel.app/'
+export const API_URL = 'https://server-test-ewnyyz5df-salivan78.vercel.app/'
