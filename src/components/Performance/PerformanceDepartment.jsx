f2ad1360843e9e58ef457e8ed2a5c83cfa7eedc7
salivan78 <s-alivan@yandex.ru>
2022-09-01 21:08:27 +0700

    link department update

--- src/components/Performance/PerformanceDepartment.jsx
+++ src/components/Performance/PerformanceDepartment.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import pd from './PerformanceDepartment.module.css'
 import {useSelector} from "react-redux";
+import {GIT_URL} from "../../config";
 
 const PerformDepartment = () => {
 
@@ -10,40 +11,40 @@ const PerformDepartment = () => {
     return (
         <div className={pd.link}>
             <div>
-                <NavLink to={`/performance/all`}>Общее</NavLink>
+                <NavLink to={`${GIT_URL}/performance/all`}>Общее</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/so`}>СО</NavLink>
+                <NavLink to={`${GIT_URL}/performance/so`}>СО</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/ou`}>ОУ</NavLink>
+                <NavLink to={`${GIT_URL}/performance/ou`}>ОУ</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/odp`}>ОДП</NavLink>
+                <NavLink to={`${GIT_URL}/performance/odp`}>ОДП</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/osfif`}>ОСФиФ</NavLink>
+                <NavLink to={`${GIT_URL}/performance/osfif`}>ОСФиФ</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/oto`}>ОТО</NavLink>
+                <NavLink to={`${GIT_URL}/performance/oto`}>ОТО</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/okp`}>ОКП</NavLink>
+                <NavLink to={`${GIT_URL}/performance/okp`}>ОКП</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/ohp`}>ОХП</NavLink>
+                <NavLink to={`${GIT_URL}/performance/ohp`}>ОХП</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/ops`}>ОПС</NavLink>
+                <NavLink to={`${GIT_URL}/performance/ops`}>ОПС</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/opf`}>ОПФ</NavLink>
+                <NavLink to={`${GIT_URL}/performance/opf`}>ОПФ</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/uso`}>УСО</NavLink>
+                <NavLink to={`${GIT_URL}/performance/uso`}>УСО</NavLink>
             </div>
             <div>
-                <NavLink to={`/performance/worker`}>Часы</NavLink>
+                <NavLink to={`${GIT_URL}/performance/worker`}>Часы</NavLink>
             </div>
             {role == 'admin' &&
             <button>Добавить цех</button>
